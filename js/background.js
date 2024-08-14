d322d90880bab1e2523054cbb1b4b8dde86a4d1e
clr4takeoff <jetblue22@o.cnu.ac.kr>
2024-08-14 16:15:17 +0900

    7.1. Adding ToDos

--- js/background.js
+++ js/background.js
@@ -6,8 +6,6 @@ const images = [
 
 const chosenImage = images[Math.floor(Math.random()*images.length)];
 
-console.log(chosenImage);
-
 const bgImage=document.createElement("img");
 
 bgImage.src=`img/${chosenImage}`;
--- js/todo.js
+++ js/todo.js
@@ -1,13 +1,23 @@
 const toDoForm = document.getElementById("todo-form");
-// 아래는 이것과 같음
-// const toDoInput = document.querySelector("#todo-form input");
+// const toDoInput = document.querySelector("#todo-form input"); 로 써도 상관 없음
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.getElementById("todo-list");
+
+function paintToDo(newTodo) {
+    // const potato = document.createElement("li"); 로 써도 상관 없음
+    const li=document.createElement("li");
+    const span=document.createElement("span");
+    li.appendChild(span);
+    span.innerText=newTodo;
+    toDoList.appendChild(li);
+}
+
 
 function handleToDoSubmit(event) {
     event.preventDefault();
     const newTodo=toDoInput.value;
     toDoInput.value="";
+    paintToDo(newTodo);
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit)
