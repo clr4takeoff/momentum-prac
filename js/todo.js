a189dcaba7d319b148b91db96213d2974388a5f2
clr4takeoff <jetblue22@o.cnu.ac.kr>
2024-08-14 16:58:46 +0900

    7.2. Deleting ToDos

--- js/todo.js
+++ js/todo.js
@@ -3,12 +3,21 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.getElementById("todo-list");
 
+function deleteToDo() {
+    const li = event.target.parentElement;
+    li.remove();
+}
+
 function paintToDo(newTodo) {
     // const potato = document.createElement("li"); 로 써도 상관 없음
     const li=document.createElement("li");
     const span=document.createElement("span");
-    li.appendChild(span);
     span.innerText=newTodo;
+    const button = document.createElement("button");
+    button.innerText="❌";
+    button.addEventListener("click",deleteToDo);
+    li.appendChild(span);
+    li.appendChild(button);
     toDoList.appendChild(li);
 }
 
